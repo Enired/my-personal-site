f2db75f9ee48a44d10937f0e6a87382d7aad70f3
Derine Banis <d.banis.13@gmail.com>
2022-08-16 15:24:55 -0700

    Add basic styling to nav bar

--- client/src/components/TopNav.jsx
+++ client/src/components/TopNav.jsx
@@ -1,15 +1,15 @@
 import { AppBar, Toolbar } from "@mui/material"
-
+import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
+import "../styles/topNav.scss"
 export const TopNav = () => {
 
   return(
     <AppBar id="main-nav-bar">
-      <Toolbar>
+        <MenuOutlinedIcon id="nav-bar-menu"/>
         <div className="title-bundle">
-        <h5 id="title-header-name">Derine Banis</h5>
-        <h6 id="title-header-occupation">Full Stack Web Developer</h6>
+          <h5 id="title-header-name">Derine Banis</h5>
+          <h6 id="title-header-occupation">Full Stack Web Developer</h6>
         </div>
-      </Toolbar>
     </AppBar>
   )
 }
