58b01e429fd023f0de3462e34449db552fa1a618
Derine Banis <d.banis.13@gmail.com>
2022-08-16 21:57:08 -0700

    Add dropdown menu to menu icon. Displayed when clicked

--- client/src/components/TopNav.jsx
+++ client/src/components/TopNav.jsx
@@ -2,20 +2,59 @@ import { AppBar, Toolbar } from "@mui/material"
 import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
 import "../styles/topNav.scss"
 import img from "../assets/logo.png"
+import { useState } from "react";
+import {Menu, MenuItem} from "@mui/material";
 export const TopNav = () => {
+
+  //FOR DROPDOWN MENU -- START
+  const [anchor, setAnchor] = useState(null)
+  const openMenu = (event) => {
+    setAnchor(event.currentTarget);
+  }
+  const closeMenu = () => {
+    setAnchor(null)
+  }
+  const open= Boolean(anchor)
+  //FOR DROPDOWN MENU -- END
 
   return(
     <AppBar id="main-nav-bar">
+
       <div id="nav-left">
-        <MenuOutlinedIcon id="nav-bar-menu"/>
+
+        <div 
+        id="nav-bar-menu" 
+        onClick={(event)=>openMenu(event)}
+        >
+        
+        <MenuOutlinedIcon />
+        </div>
+
+        <Menu
+        anchorEl={anchor}
+        open={open}
+        >
+          <MenuItem onClick={()=>{console.log('Clicked About Me Link')}}>About Me</MenuItem>
+          <MenuItem onClick={(event)=>{closeMenu(event)}}>Close</MenuItem>
+        </Menu>
+
+        
+
         <div id="title-bundle">
+
           <h5 id="title-header-name">Derine Banis</h5>
           <h6 id="title-header-occupation">Full Stack Web Developer</h6>
+
         </div>
+
       </div>
+
       <div id="nav-right">
+
         <img id="logo" src={img}></img>
+
       </div>
+
     </AppBar>
   )
 }
