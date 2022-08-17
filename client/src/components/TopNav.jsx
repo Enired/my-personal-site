48b918ea06d348292a9743788e0bba2c7cc964dd
Derine Banis <d.banis.13@gmail.com>
2022-08-16 19:31:52 -0700

    Add logo and styling to nav bar

--- client/src/components/TopNav.jsx
+++ client/src/components/TopNav.jsx
@@ -1,15 +1,21 @@
 import { AppBar, Toolbar } from "@mui/material"
 import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
 import "../styles/topNav.scss"
+import img from "../assets/logo.png"
 export const TopNav = () => {
 
   return(
     <AppBar id="main-nav-bar">
+      <div id="nav-left">
         <MenuOutlinedIcon id="nav-bar-menu"/>
-        <div className="title-bundle">
+        <div id="title-bundle">
           <h5 id="title-header-name">Derine Banis</h5>
           <h6 id="title-header-occupation">Full Stack Web Developer</h6>
         </div>
+      </div>
+      <div id="nav-right">
+        <img id="logo" src={img}></img>
+      </div>
     </AppBar>
   )
 }
