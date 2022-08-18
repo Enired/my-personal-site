c4d4f9b864c98e9e63accc72abb3d818c0f21973
Derine Banis <d.banis.13@gmail.com>
2022-08-17 20:07:02 -0700

    Setup for main area. Setup for about me section

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
-import './App.css';
-import { TopNav } from './components/TopNav';
+import "./styles/app.scss"
+
+//Importing my components
+import { TopNav} from './components/TopNav';
+import { AboutMe } from './components/AboutMe';
 
 function App() {
   return (
     <div className="App">
       <TopNav/>
+      <div id="main-area">
+      <AboutMe/>
+      </div>
     </div>
   );
 }
---
+++ client/src/components/AboutMe.jsx
@@ -0,0 +1,7 @@
+export const AboutMe = () => {
+  return(
+    <div id="about-me-section">
+      Hello
+    </div>
+  )
+}
