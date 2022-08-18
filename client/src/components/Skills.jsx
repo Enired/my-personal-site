7f526922a6bd5c3e1599a5b02d14d4b300abd7ef
Derine Banis <d.banis.13@gmail.com>
2022-08-17 22:02:44 -0700

    Add icons to skills section

--- client/src/components/Skills.jsx
+++ client/src/components/Skills.jsx
@@ -1,9 +1,33 @@
+//Importing Logos
+import javascriptLogo from "../assets/skills_icons/javascript.png"
+import rubyLogo from "../assets/skills_icons/ruby.png"
+import reactLogo from "../assets/skills_icons/react.webp"
+
+//Import Style
+import "../styles/skills.scss"
+
 export const Skills = () => {
 
   return(
     <div className="area-section" id="skills-section">
       <h1 className="section-header">Skills</h1>
       <div id="skills-icons">
+
+        <div className="skill">
+          <img src={javascriptLogo} alt="Javascript" className="skill-logo" />
+          <p className="skill-tag">Javascript</p>
+        </div>
+
+        <div className="skill">
+          <img src={rubyLogo} alt="Ruby" className="skill-logo" />
+          <p className="skill-tag">Ruby</p>
+        </div>
+        
+        <div className="skill">
+          <img src={reactLogo} alt="React" className="skill-logo" />
+          <p className="skill-tag">React</p>
+        </div>
+
       </div>
     </div>
   )
