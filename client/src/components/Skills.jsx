b3f7bec96134053b1a868b16d592c551da4d84d5
Derine Banis <d.banis.13@gmail.com>
2022-08-18 18:32:16 -0700

    Add skills to skills section

--- client/src/components/Skills.jsx
+++ client/src/components/Skills.jsx
@@ -5,6 +5,14 @@ import reactLogo from "../assets/skills_icons/react.webp"
 import pythonLogo from "../assets/skills_icons/python.png"
 import nodeLogo from "../assets/skills_icons/nodejs.webp"
 import storybookLogo from "../assets/skills_icons/storybook.png"
+import bootstrapLogo from "../assets/skills_icons/bootstrap.png"
+import cypressLogo from "../assets/skills_icons/cypress.png"
+import jestLogo from "../assets/skills_icons/jest.png"
+import jqueryLogo from "../assets/skills_icons/jquery.png"
+import materialUILogo from "../assets/skills_icons/MaterialUI.png"
+import postgressqlLogo from "../assets/skills_icons/postgresql.png"
+import railsLogo from "../assets/skills_icons/rubyonrails.png"
+import sassLogo from "../assets/skills_icons/sass.webp"
 
 
 //Import Style
@@ -45,7 +53,47 @@ export const Skills = () => {
 
         <div className="skill">
           <img src={storybookLogo} alt="Express" className="skill-logo" />
-          <p className="skill-tag">Express</p>
+          <p className="skill-tag">Storybook</p>
+        </div>
+
+        <div className="skill">
+          <img src={bootstrapLogo} alt="Bootstrap" className="skill-logo" />
+          <p className="skill-tag">Bootstrap</p>
+        </div>
+
+        <div className="skill">
+          <img src={cypressLogo} alt="Cypress" className="skill-logo" />
+          <p className="skill-tag">Cypress</p>
+        </div>
+
+        <div className="skill">
+          <img src={jestLogo} alt="Jest" className="skill-logo" />
+          <p className="skill-tag">Jest</p>
+        </div>
+
+        <div className="skill">
+          <img src={jqueryLogo} alt="JQuery" className="skill-logo" />
+          <p className="skill-tag">JQuery</p>
+        </div>
+
+        <div className="skill">
+          <img src={materialUILogo} alt="Material UI" className="skill-logo" />
+          <p className="skill-tag">Material UI</p>
+        </div>
+
+        <div className="skill">
+          <img src={postgressqlLogo} alt="PostgreSQL" className="skill-logo" />
+          <p className="skill-tag">PostgreSQL</p>
+        </div>
+
+        <div className="skill">
+          <img src={railsLogo} alt="Rails" className="skill-logo" />
+          <p className="skill-tag">Rails</p>
+        </div>
+
+        <div className="skill">
+          <img src={sassLogo} alt="SASS" className="skill-logo" />
+          <p className="skill-tag">SASS</p>
         </div>
 
       </div>
