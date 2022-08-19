fa43f00dc1b3eaeb8a27d94df50350e69376df51
Derine Banis <d.banis.13@gmail.com>
2022-08-19 15:51:23 -0700

    Add placeholder for projects. Test appearance and placement.

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -1,10 +1,27 @@
+import jungle from "../assets/project_gifs/jungle.gif"
+import "../styles/projects.scss"
 export const Projects = () => {
   return(
     <div className="area-section" id="projects-section">
       <h1 className="section-header">Projects</h1>
       <div id="all-projects">
+        
         <div className="project">
+          <h1 className="project-title">Jungle</h1>
+          <a href="https://github.com/Enired/jungle-ruby">
+          <img className="project-gif" src={jungle} alt="Jungle Gif"></img>
+          </a>
+          <p className="project-desc">E-commerce Website called Jungle. Done as a project for Lighthouse Labs Web Dev Bootcamp</p>
         </div>
+
+        <div className="project">
+          <h1 className="project-title">Jungle</h1>
+          <a href="https://github.com/Enired/jungle-ruby">
+          <img className="project-gif" src={jungle} alt="Jungle Gif"></img>
+          </a>
+          <p className="project-desc">E-commerce Website called Jungle. Done as a project for Lighthouse Labs Web Dev Bootcamp</p>
+        </div>
+
       </div>
     </div>
   )
