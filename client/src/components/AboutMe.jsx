500e448553862706460f93efe7ce702a5b82de9b
Derine Banis <d.banis.13@gmail.com>
2022-08-23 12:12:59 -0700

    Change to flex display for sections. Remove <hr> tags from about section.

--- client/src/components/AboutMe.jsx
+++ client/src/components/AboutMe.jsx
@@ -2,9 +2,7 @@ export const AboutMe = () => {
   return(
     <div className="area-section" id="about-me-section">
       <h1 className="section-header">About</h1>
-      <hr/>
       <p className="section-body" id="about-me-bio">I am a self-motivated full-stack developer with a desire for learning and development. My interest in coding is rooted in my passion for video games and attachment to the internet. With a background in the hospitality industry I have developed strong team-work and leadership skills, and look forward to bringing that forward to my next opportunity.</p>
-      <hr/>
     </div>
   )
 }
