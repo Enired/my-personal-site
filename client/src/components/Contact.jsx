ef35350eade23bb60dabbd9a641157f197d8b0bd
Derine Banis <d.banis.13@gmail.com>
2022-08-23 12:05:14 -0700

    Add links to contact section

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,14 +1,17 @@
+//Import Style
+import "../styles/contact.scss"
+
 export const Contact = () => {
   return (
     <div className="area-section" id="contact-me-section">
       <h1 className="section-header">Contact</h1>
       <p className="section-body">
-        <ul>
-          <li>LinkedIn : <a href="https://www.linkedin.com/in/derine-banis-0913/">Derine Banis</a>
+        <ul id="contact-links">
+          <li>LinkedIn : <a className="contact-link" href="https://www.linkedin.com/in/derine-banis-0913/">Derine Banis</a>
           </li>
-          <li>Email : <a href="mailto:d.banis.13@gmail.com">d.banis@gmail.com</a> </li>
+          <li>Email : <a className="contact-link" href="mailto:d.banis.13@gmail.com">d.banis@gmail.com</a> </li>
           <li>
-            GitHub : <a href="https://github.com/Enired">Enired</a>
+            GitHub : <a className="contact-link" href="https://github.com/Enired">Enired</a>
           </li>
         </ul>
       </p>
